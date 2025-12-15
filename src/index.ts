957e897136aa4466a2f074fecb231501dd3d4326
DGwebdes <alan.bilik8@gmail.com>
2025-12-15 17:29:04 +0000

    most functionalities implemented. updated compressImage fn to validate input. filter files per extension allowed.

--- src/index.ts
+++ src/index.ts
@@ -11,26 +11,23 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-// ? is there a better way to wait out for input and results?
-const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
-const welcomeMessage = chalk.bgBlack.italic(
+const EXTENSIONS = [".jpeg", ".jpg", ".png"];
+const ACCEPTED_FORMATS = chalk.bgRed(`Accepted formats: JPEG, JPG, PNG`);
+const WELCOME_MESSAGE = chalk.bgBlack.italic(
     `Image.* to Image.webp Compressor. \n`
 );
 const OUTPUT: string = "./output";
-let input: string = "";
 
 async function welcome() {
-    const intro = chalk.blue.bold(welcomeMessage);
+    const intro = chalk.blue.bold(WELCOME_MESSAGE);
     console.log(intro);
 }
 
 async function howToUse() {
     const note = chalk.red.italic(`If output source does't exist. Create one.`);
     const howTo = chalk.green(`
-        Usage > 
-        run comp-cli
-        source of image/s
-        output to output/ directory
+        Usage > ./directory_path 
+        outputs to 'output/' directory
         \n
         ${note}
         `);
@@ -39,49 +36,63 @@ async function howToUse() {
 // TODO: Validate user input
 async function inputSource() {
     rl.question("Source of Images\n ", (inputDir) => {
-        if (!inputDir) {
-            console.log(chalk.bgRed("Must provide a source directory path"));
-            rl.close();
-            return;
-        }
-        input = inputDir.trim();
+        const input = inputDir.trim();
         compressImages(input);
-        rl.close();
     });
 }
 
 async function compressImages(argInput: string) {
     // Takes the images from the directory indicated in the first argument
-    if (!fs.existsSync(argInput)) {
-        console.log(chalk.bgYellowBright.black(`Cannot find path provided`));
-        return;
-    }
+    try {
+        /**
+         * ! Checks if input is valid
+         */
+        if (!fs.existsSync(argInput)) {
+            console.log(
+                chalk.bgYellowBright.black(`Cannot find path provided`)
+            );
+            return;
+        }
+        if (!fs.statSync(argInput).isDirectory()) {
+            console.log(chalk.bgRed("Path provided is not a directory"));
+            return;
+        }
+        /**
+         * * Initialized files array and checks if output exists
+         */
 
-    let files = fs.readdirSync(`${argInput}`, { recursive: false });
-    console.log("Reading files");
-    files.forEach((file) => {
-        const filename = typeof file === "string" ? file : file.toString();
-        const { name, ext } = path.parse(filename);
-        const image = sharp(path.join(argInput, filename));
-
-        // Compresses images and converts to webp
-        image.resize({ width: 1080 }).toFormat("webp", { quality: 80 });
-
+        let files = fs.readdirSync(`${argInput}`);
         if (!fs.existsSync(OUTPUT)) {
             fs.mkdirSync(OUTPUT);
         }
-        image
-            .toFile(path.join(OUTPUT, `${name}.webp`))
-            .then(() => {
-                console.log("Compressed!");
-            })
-            .catch((err) => console.log(err));
-    });
+        const jobs = files
+            .filter((extensions) =>
+                EXTENSIONS.includes(path.extname(extensions))
+            )
+            .map((file) => {
+                const filename = file;
+                const { name, ext } = path.parse(filename);
+
+                // Compresses images and converts to webp
+                const image = sharp(path.join(argInput, filename));
+
+                return image
+                    .resize({ width: 1080 })
+                    .toFormat("webp", { quality: 80 })
+                    .toFile(path.join(OUTPUT, `${name}.webp`));
+            });
+        await Promise.all(jobs);
+    } catch (error) {
+        console.error(chalk.red(`Sorry, something went wrong ${error}`));
+        process.exit(1);
+    } finally {
+        console.log(chalk.green(`Your file have been compressed.`));
+        rl.close();
+    }
     // Returns compressed images to either the directory destination indicated on the output argument or the same input directory if no output is given
 }
 
 console.clear();
 await welcome();
 await howToUse();
-await sleep();
 await inputSource();
